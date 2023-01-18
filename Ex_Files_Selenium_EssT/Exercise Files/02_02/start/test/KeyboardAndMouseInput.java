30291a84922eb80d03704e398000ad6c4f5a822d
Irina <irkaz@hotmail.com>
2023-01-17 23:21:15 -0800

    Initial commit1

---
+++ Ex_Files_Selenium_EssT/Exercise Files/02_02/start/test/KeyboardAndMouseInput.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class KeyboardAndMouseInput {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:/Users/irkaz/Downloads/chromedriver_win32/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://formy-project.herokuapp.com/keypress");
+
+        WebElement name = driver.findElement(By.id("name"));
+        name.click();//to set focus
+        name.sendKeys("Irina");
+        driver.quit();
+    }
+}
